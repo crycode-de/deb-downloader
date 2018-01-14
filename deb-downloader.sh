2af43d9718374dc8645d47d51f9eb5577623485f
Peter Müller <peter@crycode.de>
2018-01-14 11:38:08 +0100

    Updated license information

--- deb-downloader.sh
+++ deb-downloader.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
-#########################
-#    deb-downloader     #
-# (c) 2018 Peter Müller #
-#  https://crycode.de   #
-#########################
+###################################
+#         deb-downloader          #
+# Copyright (C) 2018 Peter Müller #
+#       https://crycode.de        #
+###################################
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
 
 # function to display the help text
 function showHelp {
@@ -134,11 +149,11 @@ function packageDownload {
 
 # function for the main process
 function main {
-  echo "#########################"
-  echo "#    deb-downloader     #"
-  echo "# (c) 2018 Peter Müller #"
-  echo "#  https://crycode.de   #"
-  echo "#########################"
+  echo "###################################"
+  echo "#         deb-downloader          #"
+  echo "# Copyright (C) 2018 Peter Müller #"
+  echo "#       https://crycode.de        #"
+  echo "###################################"
   echo
 
   # show help if no arguments given
